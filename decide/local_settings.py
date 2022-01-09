bd3b0436f29e6c212be01d7241c80a56270d2261
alvechdel <alvechdel@alum.us.es>
2022-01-09 14:12:51 +0100

    fix-final-config-docker-heroku-003
    Delete config local_settings.py

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -15,30 +15,5 @@ CORS_ORIGIN_ALLOW_ALL = True
     'voting',
 ]'''
 
-# APIS = {
-#     'authentication': 'http://localhost:8000',
-#     'base': 'http://localhost:8000',
-#     'booth': 'http://localhost:8000',
-#     'census': 'http://localhost:8000',
-#     'mixnet': 'http://localhost:8000',
-#     'postproc': 'http://localhost:8000',
-#     'store': 'http://localhost:8000',
-#     'visualizer': 'http://localhost:8000',
-#     'voting': 'http://localhost:8000',
-# }
-
-# BASEURL = 'http://localhost:8000'
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'djongo',
-#         'NAME': 'decide',
-#         'CLIENT': {
-#            'host': '127.0.0.1',
-#         }
-
-#     }
-# }
-
 # number of bits for the key, all auths should use the same number of bits
 KEYBITS = 256
